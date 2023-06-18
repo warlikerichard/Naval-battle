ff2bc6198067cdb0588507f0d510f0da379bf18c
warlikerichard <warlikerichard@hotmail.com>
2023-06-18 15:52:52 -0300

    Minor changes and added funtion to Tabuleiro

--- src/imd/ufrn/br/modelo/Tabuleiro.java
+++ src/imd/ufrn/br/modelo/Tabuleiro.java
@@ -7,11 +7,7 @@ public class Tabuleiro {
 	// Baseado na lista de embarcações atual, o tabuleiro (mapa) será atualizado.
 	// Note que esta lista não mudará uma vez que o jogo for iniciado.
 	private HashMap<Integer, Navio> navios = new HashMap<Integer, Navio>();
-	private Bloco[][] mapa = new Bloco[10][10]; // Mapa inicializado com blocos com valores 0, indicando que não há navios no mesmo
-												// Use 1 para indicar que um navio em bom estado ocupa este bloco
-												// Use 2 para indicar que um navio destruido ocupa este bloco
-												// Cada bloco terá uma chave que indica a qual navio o mesmo pertence, tornando
-												// a manipulação de estados mais fácil
+	private Bloco[][] mapa = new Bloco[10][10];
 	
 	public Tabuleiro() {
 		for (int x = 0; x < 10 ; x++) {
@@ -19,14 +15,12 @@ public class Tabuleiro {
 				this.mapa[x][y] = new Bloco();
 			}
 		}
-		
-		this.addNavio(new Corveta(0,  new int[]{0, 0}));
 	}
 	
 	public void print() {
 		for (int x = 0; x < 10 ; x++) {
 			for (int y = 0; y < 10 ; y++) {
-				System.out.print(this.mapa[x][y].getEstado());
+				System.out.print(this.mapa[x][y].getEstado() == 2 ? "*" : this.mapa[x][y].getEstado());
 			}
 			System.out.println();
 		}
@@ -50,5 +44,10 @@ public class Tabuleiro {
 	
 	public void setMapa(Bloco[][] mapa) {
 		this.mapa = mapa;
+	}
+	
+	//Updates the blocks according to the current ships stored, so the states of the blocks won't just be 0
+	public void confirmShips() {
+		
 	}
 }
