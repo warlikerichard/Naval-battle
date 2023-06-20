38af5ce963c563a77ea458643fa025e726eca074
ralf998 <93091507+ralf998@users.noreply.github.com>
2023-06-20 14:07:22 -0300

    Test intellij

--- src/imd/ufrn/br/visao/Testes.java
+++ src/imd/ufrn/br/visao/Testes.java
@@ -1,11 +1,13 @@
 package imd.ufrn.br.visao;
 
 import imd.ufrn.br.modelo.Tabuleiro;
+import imd.ufrn.br.modelo.TabuleiroCPU;
+import imd.ufrn.br.modelo.TabuleiroPlayer;
 
 public class Testes {
 	public static void main(String args[]) {
-		Tabuleiro tabuleiroPlayer = new Tabuleiro();
-		Tabuleiro tabuleiroCPU = new Tabuleiro();
+		Tabuleiro tabuleiroPlayer = new TabuleiroPlayer();
+		Tabuleiro tabuleiroCPU = new TabuleiroCPU();
 		
 		tabuleiroPlayer.print();
 		System.out.println();
