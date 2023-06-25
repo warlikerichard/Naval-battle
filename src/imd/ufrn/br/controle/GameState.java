e884948140845ba3fd99519bc250e24e07e0cf00
warlikerichard <warlikerichard@hotmail.com>
2023-06-25 20:06:25 -0300

    Added GameState

---
+++ src/imd/ufrn/br/controle/GameState.java
@@ -0,0 +1,10 @@
+package imd.ufrn.br.controle;
+
+public enum GameState {
+	MENU,
+	SHIP_SELECTION,
+	PLAYING,
+	WIN,
+	GAME_OVER,
+	EXIT_GAME
+}
--- src/imd/ufrn/br/modelo/TabuleiroPlayer.java
+++ src/imd/ufrn/br/modelo/TabuleiroPlayer.java
@@ -14,5 +14,6 @@ public class TabuleiroPlayer extends Tabuleiro {
 			System.out.println();
 		}
 		System.out.println();
+		
 	}
 }
