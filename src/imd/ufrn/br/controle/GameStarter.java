d212f76063359d7ee4868df0ac2a08d84de8c3f0
warlikerichard <warlikerichard@hotmail.com>
2023-06-25 19:23:00 -0300

    Menu adicionado

--- src/imd/ufrn/br/controle/GameStarter.java
+++ src/imd/ufrn/br/controle/GameStarter.java
@@ -1,12 +1,13 @@
 package imd.ufrn.br.controle;
-
 import imd.ufrn.br.modelo.*;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class GameStarter {
 	private TabuleiroCPU tabuleiroCPU;
 	private TabuleiroPlayer tabuleiroPlayer;
-	
-	
+	private File controls = new File("src/imd/ufrn/br/controle/controls.txt");
 	
 	private GameState state = GameState.MENU;
 	
@@ -41,6 +42,35 @@ public class GameStarter {
 
 	public void setTabuleiroPlayer(TabuleiroPlayer tabuleiroPlayer) {
 		this.tabuleiroPlayer = tabuleiroPlayer;
+	}
+	
+	public void printMenu() {
+		System.out.println("================== BATALHA NAVAL =================\n");
+		tabuleiroPlayer.print();
+		System.out.println("1- Iniciar\t2- Controles\t3- Sair");
+		System.out.println();
+		
+	}
+	
+	public void printControles() {
+		try {
+            Scanner scanner = new Scanner(controls);
+            Scanner read = new Scanner(System.in);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+            
+            System.out.println("Pressione enter para voltar.");
+            read.nextLine();
+            read.close();
+            scanner.close();
+            
+            printMenu();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 	}
 
 }
